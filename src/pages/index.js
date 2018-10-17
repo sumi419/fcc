8fd802c3707360904228f93af67557bc5d278624
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-10-17 15:34:11 -0700

    First commit message :)

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello world, my name is Sumayyah</h1>
+    <p>I am Full Stack Web Developer</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
